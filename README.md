0c870be647d51c83a6b1960e180c66850d6885cd
spoorcc <ben.spoor@gmail.com>
2017-04-25 09:58:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GDB kernel for Jupyter Notebooks
+
+This kernel can be used to show GDB commands in Jupyter Notebooks
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
+
+## Development
+
+TODO
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jupyter_c_kernel',
+      version='0.0.1',
+      description='GDB kernel for Jupyter',
+      author='Ben Spoor',
+      author_email='gdb-kernel@spoor.cc',
+      url='https://github.com/spoorcc/jupyter-gdb-kernel/',
+      download_url='https://github.com/spoorcc/jupyter-gdb-kernel/tarball/0.0.1',
+      packages=['jupyter_gdb_kernel'],
+      keywords=['jupyter', 'kernel', 'gdb']
+      )
